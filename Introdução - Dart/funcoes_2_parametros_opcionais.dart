203299cafc2c64acdb5a38847fea5d650626b605
traue <t.traue@gmail.com>
2021-09-03 11:31:38 -0300

    Aulas novas

---
+++ Introdução - Dart/funcoes_2_parametros_opcionais.dart
@@ -0,0 +1,13 @@
+void main() {
+  criarBotao(
+    texto: "Vai",
+    altura: 100
+  );
+}
+
+void criarBotao({String texto, String cor, double largura, double altura}) {
+  print("Criei um botão assim: ");
+  print("Texto: $texto");
+  print("Cor: ${cor ?? "Cinza"}");
+  print("Tamanho: ${largura ?? 20} por ${altura ?? 5}");
+}
