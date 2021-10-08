b7abe211c9dd28ca7291c992c49d22fb8e1c2adf
Prof. Thiago G. Traue <t.traue@gmail.com>
2021-10-08 11:39:05 -0300

    Projeto uni_clima

---
+++ uni_clima/lib/screens/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:searchable_dropdown/searchable_dropdown.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final List<String> _cidades = [
+    "Aracaju",
+    "Belém",
+    "Belo Horizonte",
+    "Boa Vista",
+    "Brasilia",
+    "Campo Grande",
+    "Cuiaba",
+    "Curitiba",
+    "Florianopolis",
+    "Fortaleza",
+    "Goiânia",
+    "João Pessoa",
+    "Macapá",
+    "Maceió",
+    "Manaus",
+    "Natal",
+    "Palmas",
+    "Porto Alegre",
+    "Porto Velho",
+    "Recife",
+    "Rio Branco",
+    "Rio de Janeiro",
+    "Salvador",
+    "São Luiz",
+    "São Paulo",
+    "Teresina",
+    "Vitória"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
